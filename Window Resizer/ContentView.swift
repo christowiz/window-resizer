c547fb67f09c52ab12b1f7301bd889a6aa854111
Christopher Gwizdala <christopher.gwizdala@gmail.com>
2026-02-08 15:11:24 -0500

    Initial Commit

---
+++ Window Resizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Window Resizer
+//
+//  Created by Christopher Gwizdala on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Window Resizer/Window_ResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Window_ResizerApp.swift
+//  Window Resizer
+//
+//  Created by Christopher Gwizdala on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Window_ResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
